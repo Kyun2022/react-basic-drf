e31e5a60d00805b2d2a16b0a44c76002a117c6af
kyun <fm13071303@rx.tnc.ne.jp>
2023-12-03 01:06:38 +0900

    ✨ 　学習終了

--- src/components/DrfApiFetch.js
+++ src/components/DrfApiFetch.js
@@ -1,9 +1,137 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const DrfApiFetch = () => {
-  return (
-    <div>DrfApiFetch</div>
-  )
-}
+  const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState([]);
+  const [editedTask, setEditedTask] = useState({ id: "", title: "" });
+  const [id, setId] = useState(1);
 
-export default DrfApiFetch
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/tasks/", {
+        headers: {
+          Authorization: "Token e8903857e786403f71948f8841c722aa3e20d0bf",
+        },
+      })
+      .then((res) => {
+        setTasks(res.data);
+      });
+  }, []);
+
+  const getTask = () => {
+    axios
+      .get(`http://127.0.0.1:8000/api/tasks/${id}/`, {
+        headers: {
+          Authorization: "Token e8903857e786403f71948f8841c722aa3e20d0bf",
+        },
+      })
+      .then((res) => {
+        setSelectedTask(res.data);
+      });
+  };
+
+  const deleteTask = (id) => {
+    axios
+      .delete(`http://127.0.0.1:8000/api/tasks/${id}/`, {
+        headers: {
+          Authorization: "Token e8903857e786403f71948f8841c722aa3e20d0bf",
+        },
+      })
+      .then((res) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+        setSelectedTask([]);
+      });
+  };
+
+  const newTask = (task) => {
+    const data = {
+      title: task.title,
+    };
+
+    axios
+      .post(`http://127.0.0.1:8000/api/tasks/`, data, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Token e8903857e786403f71948f8841c722aa3e20d0bf",
+        },
+      })
+      .then((res) => {
+        setTasks([...tasks, res.data]);
+        setEditedTask({ id: "", title: "" });
+      });
+  };
+
+  const editTask = (task) => {
+    axios
+      .put(`http://127.0.0.1:8000/api/tasks/${task.id}/`, task, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Token e8903857e786403f71948f8841c722aa3e20d0bf",
+        },
+      })
+      .then((res) => {
+        setTasks(
+          tasks.map((task) => (task.id === editedTask.id ? res.data : task))
+        );
+        setEditedTask({ id: "", title: "" });
+      });
+  };
+
+  const handleInputChange = () => (evt) => {
+    const value = evt.target.value;
+    const name = evt.target.name;
+    setEditedTask({ ...editedTask, [name]: value });
+  };
+
+  return (
+    <>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.title} {task.id}
+            <button onClick={() => deleteTask(task.id)}>
+              <i className="fas fa-trash-alt"></i>
+            </button>
+            <button onClick={() => setEditedTask(task)}>
+              <i className="fas fa-pen"></i>
+            </button>
+          </li>
+        ))}
+      </ul>
+      Set id <br />
+      <input
+        type="text"
+        value={id}
+        onChange={(evt) => {
+          setId(evt.target.value);
+        }}
+      />
+      <br />
+      <button type="button" onClick={() => getTask()}>
+        Get Task
+      </button>
+      {/* <button type="button" onClick={() => deleteTask()}>
+        Delete Task
+      </button> */}
+      <h3>
+        {selectedTask.title} {selectedTask.id}
+      </h3>
+      <input
+        type="text"
+        name="title"
+        value={editedTask.title}
+        onChange={handleInputChange()}
+        placeholder="New task ?"
+        required
+      />
+      {editedTask.id ? (
+        <button onClick={() => editTask(editedTask)}>Update</button>
+      ) : (
+        <button onClick={() => newTask(editedTask)}>Create</button>
+      )}
+    </>
+  );
+};
+
+export default DrfApiFetch;
